96e5512eb99f5bdfd3d4d44cf6327ce5c43a9ae6
williamjfarmer@tiscali.co.uk <williamjfarmer@tiscali.co.uk@4ec08b08-1c92-11df-90e4-2d055072c7ef>
2014-03-12 20:55:35 +0000

    Add linux source files

---
+++ trunk/linux/tuner.h
@@ -0,0 +1,119 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Tuner - A tuner written in C.
+//
+//  Copyright (C) 2014  Bill Farmer
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//  Bill Farmer  william j farmer [at] yahoo [dot] co [dot] uk.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <gtk/gtk.h>
+#include <pthread.h>
+
+#include <alsa/asoundlib.h>
+
+enum
+  {MARGIN = 10};
+
+// Structs
+
+typedef struct
+{
+    float f;
+    float fr;
+    int n;
+} maximum;
+
+typedef struct
+{
+    GtkWidget *widget;
+    float *data;
+    int length;
+} Scope;
+
+typedef struct
+{
+    GtkWidget *widget;
+    int length;
+    int expand;
+    int count;
+    gboolean zoom;
+    float f;
+    float r;
+    float l;
+    float h;
+    float *data;
+    float *values;
+} Spectrum;
+
+typedef struct
+{
+    GtkWidget *widget;
+    maximum *maxima;
+    float f;
+    float fr;
+    float c;
+    gboolean lock;
+    gboolean zoom;
+    gboolean multiple;
+    int count;
+    int n;
+} Display;
+
+typedef struct
+{
+    GtkWidget *widget;
+    gboolean changed;
+    gboolean enable;
+    int colours;
+    float c;
+} Strobe;
+
+typedef struct
+{
+    GtkWidget *widget;
+    GtkWidget *slider; 
+    float c;
+} Meter;
+
+typedef struct
+{
+    struct
+    {
+	GtkWidget *sample;
+	GtkWidget *actual;
+    } status;
+
+    struct
+    {
+	GtkWidget *reference;
+    } preferences;
+} Legend;
+
+typedef struct
+{
+    GtkWidget *zoom;
+    GtkWidget *lock;
+    GtkWidget *strobe;
+    GtkWidget *filter;
+    GtkWidget *multiple;
+    GtkWidget *downsample;
+} Check;
+
+int scope_draw_callback(GtkWidget *, GtkWindow *);
+int quit_clicked(GtkWidget *, GtkWindow *);
