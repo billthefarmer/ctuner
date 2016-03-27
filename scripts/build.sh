a537f766c76359656b1acfc4173b73e17862bf4c
Bill Farmer <williamjfarmer@yahoo.co.uk>
2016-03-27 15:26:30 +0100

    Update build script - no linux alsa

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,9 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     cd windows
     make  GCC=i586-mingw32msvc-gcc WINDRES=i586-mingw32msvc-windres
 
-    cd ../linux
-    ./configure
-    make
+#    cd ../linux
+#    ./configure
+#    make
 fi
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
