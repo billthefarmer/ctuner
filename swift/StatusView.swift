369fa0b9f32d1d3e05b773a929154ae8bee7d520
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-03-18 21:16:14 +0000

    Update swift code

--- swift/StatusView.swift
+++ swift/StatusView.swift
@@ -5,6 +5,22 @@
 //  Created by Bill Farmer on 08/10/2017.
 //  Copyright © 2017 Bill Farmer. All rights reserved.
 //
+//  Created by Bill Farmer on 18/02/2018.
+//  Copyright © 2018 Bill Farmer. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import Cocoa
 
