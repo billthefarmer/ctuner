cc7a0c43510042af9cf13c58f661fd94a922c0b1
Bill Farmer <williamjfarmer@yahoo.co.uk>
2018-06-25 10:47:04 +0100

    Update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -ev
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt update
-    sudo apt install mingw-w64
+    sudo apt install g++-mingw-w64
     cd windows
     make GXX=x86_64-w64-mingw32-g++ WINDRES=x86_64-w64-mingw32-windres
 
