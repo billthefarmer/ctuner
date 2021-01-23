29a6fc257871d483b11f90e4a0d14dc844c5053e
Bill Farmer <williamjfarmer@yahoo.co.uk>
2021-01-23 16:10:19 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,15 +5,4 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     sudo apt install g++-mingw-w64
     cd windows
     make GXX=x86_64-w64-mingw32-g++ WINDRES=x86_64-w64-mingw32-windres
-
-#    cd ../linux
-#    ./configure
-#    make
-fi
-
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    cd mac
-    make
-    cd ../swift
-    make
 fi
