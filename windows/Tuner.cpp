c655e614618e4bb2c293b6e07ab9045ad2d05af6
Bill Farmer <williamjfarmer@yahoo.co.uk>
2021-07-27 14:22:35 +0100

    Update downsampling

--- windows/Tuner.cpp
+++ windows/Tuner.cpp
@@ -4373,7 +4373,7 @@ VOID WaveInData(WPARAM wParam, LPARAM lParam)
 	    x2[i] = 0.0;
 
 	    for (int j = 0; j < 2; j++)
-		x2[i] += xa[(i * 2) + j] / 2.0;
+		x2[i] += xa[(i * 2) + j];
 	}
 
 	// x3 = xa << 3
@@ -4382,7 +4382,7 @@ VOID WaveInData(WPARAM wParam, LPARAM lParam)
 	    x3[i] = 0.0;
 
 	    for (int j = 0; j < 3; j++)
-		x3[i] += xa[(i * 3) + j] / 3.0;
+		x3[i] += xa[(i * 3) + j];
 	}
 
 	// x4 = xa << 4
@@ -4391,7 +4391,7 @@ VOID WaveInData(WPARAM wParam, LPARAM lParam)
 	    x4[i] = 0.0;
 
 	    for (int j = 0; j < 4; j++)
-		x2[i] += xa[(i * 4) + j] / 4.0;
+		x4[i] += xa[(i * 4) + j];
 	}
 
 	// x5 = xa << 5
@@ -4400,23 +4400,16 @@ VOID WaveInData(WPARAM wParam, LPARAM lParam)
 	    x5[i] = 0.0;
 
 	    for (int j = 0; j < 5; j++)
-		x5[i] += xa[(i * 5) + j] / 5.0;
+		x5[i] += xa[(i * 5) + j];
 	}
 
 	// Add downsamples
 	for (unsigned int i = 1; i < Length(xa); i++)
 	{
-	    if (i < Length(x2))
-		xa[i] += x2[i];
-
-	    if (i < Length(x3))
-		xa[i] += x3[i];
-
-	    if (i < Length(x4))
-		xa[i] += x4[i];
-
-	    if (i < Length(x5))
-		xa[i] += x5[i];
+            xa[i] *= (i < Length(x2))? x2[i]: 0.0;
+            xa[i] *= (i < Length(x3))? x3[i]: 0.0;
+            xa[i] *= (i < Length(x4))? x4[i]: 0.0;
+            xa[i] *= (i < Length(x5))? x5[i]: 0.0;
 
 	    // Recalculate differences
 	    dx[i] = xa[i] - xa[i - 1];
